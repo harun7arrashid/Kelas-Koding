8c0d35a29e75148582b346df1c7392cd592382e5
harun7arrashid <harun4arrashid@gmail.com>
2022-01-07 20:34:52 +0700

    Layout Fragment Register

---
+++ app/src/main/java/id/ngulemkuy/kelaskoding/ui/user/RegisterDetailFragment.kt
@@ -0,0 +1,15 @@
+package id.ngulemkuy.kelaskoding.ui.user
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import id.ngulemkuy.kelaskoding.R
+
+class RegisterDetailFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_register_detail, container, false)
+    }
+}
